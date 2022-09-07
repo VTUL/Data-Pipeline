2ad966ff89b066df997562c32add9edb370a7f35
j puryear <inkling@vt.edu>
2022-09-07 10:07:55 -0400

    add enigma.py to repo and packaging script

--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
 dp=deploymentpackage.zip
 
+rm $dp
 cd $VIRTUAL_ENV/lib/python3.8/site-packages/
 pwd
 zip -r9 $OLDPWD/$dp *
 cd $OLDPWD
 zip -gr $dp lambda_function.py libinsights_allref \
-    requirements.txt
+    requirements.txt enigma.py
